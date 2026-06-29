439e50f2b0112049fc48e8cfef594fa0f86ac865
krishnanunni6707 <24bco304@adishankara.ac.in>
2026-06-29 21:54:41 +0530

    updated the logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Smart Campus Printing System",
   description: "Efficient campus printing management",
-  icons: "/logo1.png"
+  icons: "/logo.png"
 };
 
 export default function RootLayout({
